0b41a1b158f3d68c4358313d1baf80fb3555f11d
CIPHER-EDU <ciphereduuz@gmail.com>
2024-01-21 18:53:00 +0500

    ad

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name = 'kategoriya nomi')
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 255, verbose_name='post title')
+    content = models.TextField(verbose_name='post kontenti')
+    image = models.ImageField(upload_to='post-image/')
+    date = models.DateTimeField(auto_now=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+
+    def __str__(self):
+        return self.title
