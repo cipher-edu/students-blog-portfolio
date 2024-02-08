b202568753e1060c27d0029cf4056372d26897e7
CIPHER-EDU <ciphereduuz@gmail.com>
2024-02-08 21:45:38 +0500

    Add

--- core/forms.py
+++ core/forms.py
@@ -7,5 +7,9 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = '__all__'
         widgets = {
-            'date': forms.DateTimeInput(attrs={'type':'datetime-local'})
+            'date': forms.DateTimeInput(attrs={'type':'datetime-local', 'class':'form-control col-4'}),
+            'category': forms.Select(attrs={'class':'form-control col-4'}),
+            'title': forms.TextInput(attrs={'class':'form-control col-4'}),
+            'content': forms.Textarea(attrs={'class':'form-control col-6'}),
+            'image': forms.FileInput(attrs={'class': 'form-control col-4','enctype': 'multipart/form-data'}),
         }
